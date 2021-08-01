4da35f3d4364d8c5960d56629ce5ccdc6f381d72
marcio da silva <marcioadsworks@gmail.com>
2021-08-01 12:17:54 -0300

    creating a formGroup for signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { validateLocaleAndSetLanguage } from "typescript";
 
 @IonicPage()
 @Component({
@@ -7,7 +9,44 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "signup.html",
 })
 export class SignupPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  formGroup: FormGroup;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder
+  ) {
+    this.formGroup = formBuilder.group({
+      name: [
+        "Joaquim",
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(120),
+        ],
+      ],
+      email: ["joaquim@gmail.com", [Validators.required, Validators.email]],
+      type: ["1", [Validators.required]],
+      cpfOrCnpj: [
+        "02314465350",
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(14),
+        ],
+      ],
+      password: ["123", [Validators.required]],
+      street: ["rua 04", [Validators.required]],
+      number: ["350", [Validators.required]],
+      complement: ["ap 203", []],
+      neighborhood: ["pedras", []],
+      zipcode: ["60878050", [Validators.required]],
+      phone1: ["085997061756", [Validators.required]],
+      phone2: ["", []],
+      phone3: ["", []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]],
+    });
+  }
 
   signupUser() {
     console.log("enviado");
