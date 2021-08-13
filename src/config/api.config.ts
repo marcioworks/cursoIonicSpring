d7dc4ee22fe2b71f93aeb6d60104454311cab383
marcio da silva <marcioadsworks@gmail.com>
2021-08-12 23:22:06 -0300

    testing app on celphone

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080",
+  baseUrl: "https://api-spring-java.herokuapp.com",
   baseBucketUrl: "https://curso-spring-java-ionic.s3.amazonaws.com",
 };
